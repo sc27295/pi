a819288e2082480b1f6777a6306bd586738342f9
sc27295 <31549853+sc27295@users.noreply.github.com>
2017-09-03 21:47:13 -0400

    Update Pi.java

--- src/Pi.java
+++ src/Pi.java
@@ -1,3 +1,11 @@
+/**
+ * @author Siam-Al-mer Chowdhury
+ * CSC 201-004N
+ * Assignment 1
+ * Problem 1.70
+ * Create a program that displays the result of 4 * (1 - 1 /3 + 1 /5 - 1 /7 + 1 /9 - 1 /11 ) 
+ and 4 * (1 - 1 /3 + 1/ 5 - 1 /7 + 1 /9 - 1 /11 + 1 /13 ) 
+ */
 import java.util.Scanner;
 public class Pi {
     public static void main(String[] args){
